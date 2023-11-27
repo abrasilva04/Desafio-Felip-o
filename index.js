f39f0b9aa1c7cce0d9bd8848c3007b29b1f5df6a
abrasilva04 <abraaaosilvaalves@gmail.com>
2023-11-27 14:14:02 -0300

    latest commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("olá");
+let hero = "Berzek";
+let xp = 11001;
+
+if (xp <= 1000) {
+  console.log("O herói de nome " + hero + " Está no nível de Ferro")
+} else if ((xp >= 1001) && (xp <= 2000)) {
+  console.log("O herói de nome " + hero + " Está no nível de Bronze")
+} else if ((xp >= 2001) && (xp <= 5000)) {
+  console.log("O herói de nome " + hero + " Está no nível de Prata")
+} else if ((xp >= 6001) && (xp <= 7000)) {
+  console.log("O herói de nome " + hero + " Está no nível de Ouro")
+} else if ((xp >= 7001) && (xp <= 8000)) {
+  console.log("O herói de nome " + hero + " Está no nível de Platina")
+} else if ((xp >= 8001) && (xp <= 9000)) {
+  console.log("O herói de nome " + hero + " Está no nível de Ascendente")
+} else if ((xp >= 9001) && (xp <= 10000)) {
+  console.log("O heroi de nome " + hero + " Está no nível de Imortal")
+} else if (xp >= 10001) {
+  console.log("O heroi de nome " + hero + " Está no nível de Radiante")
+}
